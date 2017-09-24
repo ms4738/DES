afa9b8879319d73deaedc19b7824dec00a0341b2
bjcrane <bjcrane2013@gmail.com>
2017-09-24 15:56:01 -0500

    Functioning input and output for encryption
    woot woot

--- DES.cpp
+++ DES.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
 
 
@@ -17,18 +18,24 @@ int main()
 
             string output;
             if (plainTextFileStream.is_open()) {
-                // cout << plainTextFileName << "File is open" << endl;
-                while (!plainTextFileStream.eof()) {
-                    plainTextFileStream >> output;
-                    cout << output;
-                    // TODO: Open Output File
+                char c;
+                vector<char> plainText;
+                while (plainTextFileStream.get(c)) {
+                    plainText.push_back(c);
+                    ofstream cipherTextFileStream;
+                    cipherTextFileStream.open("ct.txt", std::ofstream::out | std::ofstream::trunc);
+                    for(int i=0; i < plainText.size(); i++){
+                        cipherTextFileStream << plainText[i];
+                    }
+                    cipherTextFileStream.close();
                 }
+                plainTextFileStream.close();
             }
             else {
                 cout << "The file was not able to be opened." << endl;
+                plainTextFileStream.close();
             }
-            plainTextFileStream.close();
-            break;
+            return 0;
         }
         else if (mode == 'd'){
             // TODO: fill in
