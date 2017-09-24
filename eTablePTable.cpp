becbf8cd3bfcc36c71bd0983a2f44e21590982c0
Geoiv <geoiv04@gmail.com>
2017-09-24 14:26:17 -0500

    E table/P table refactored to work with vectors.

--- eTablePTable.cpp
+++ eTablePTable.cpp
@@ -1,39 +1,40 @@
 #include <iostream>
 #include <bitset>
+#include<vector>
 using namespace std;
 
-bitset eTablePerm(bitset inputArray[])
+vector<bool> eTablePerm(vector<bool> inputVector)
 {
-  short outputArraySize = 48;
-  short eTable[outputArraySize] = {32, 1,  2,  3,  4,  5,
-                                   4,  5,  6,  7,  8,  9,
-                                   12, 13, 14, 15, 16, 17,
-                                   16, 17, 18, 19, 20, 21,
-                                   20, 21, 22, 23, 24, 25,
-                                   24, 25, 26, 27, 28, 29,
-                                   28, 29, 30, 31, 32, 1};
+  short outputVectorSize = 48;
+  short eTable[outputVectorSize] = {32, 1,  2,  3,  4,  5,
+                                    4,  5,  6,  7,  8,  9,
+                                    12, 13, 14, 15, 16, 17,
+                                    16, 17, 18, 19, 20, 21,
+                                    20, 21, 22, 23, 24, 25,
+                                    24, 25, 26, 27, 28, 29,
+                                    28, 29, 30, 31, 32, 1};
+  vector<bool> outputVector;
 
-  bitset<outputArraySize> outputArray; = (char*)malloc(outputArraySize * sizeof(char));
-  for (short i = 0; i < outputArraySize; i++)
+  for (short i = 0; i < outputVectorSize; i++)
   {
-    outputArray[i] = inputArray[eTable[i]];
+    outputVector.push_back(inputVector.at(eTable[i]));
   }
-  return outputArray;
+  return outputVector;
 }
 
-char* pTablePerm(char inputArray[])
+vector<bool> pTablePerm(vector<bool> inputVector)
 {
-  short outputArraySize = 32;
-  short pTable[outputArraySize] = {16, 7,  20, 21, 29, 12, 28, 17,
-                                   1,  15, 23, 26, 5,  18, 31, 10,
-                                   2,  8,  24, 14, 32, 27, 3,  9,
-                                   19, 13, 30, 6,  22, 11, 4,  25};
-  char* outputArray = (char*)malloc(outputArraySize * sizeof(char));
-  for (short i = 0; i < outputArraySize; i++)
+  short outputVectorSize = 32;
+  short pTable[outputVectorSize] = {16, 7,  20, 21, 29, 12, 28, 17,
+                                    1,  15, 23, 26, 5,  18, 31, 10,
+                                    2,  8,  24, 14, 32, 27, 3,  9,
+                                    19, 13, 30, 6,  22, 11, 4,  25};
+  vector<bool> outputVector;
+  for (short i = 0; i < outputVectorSize; i++)
   {
-    outputArray[i] = inputArray[pTable[i]];
+    outputVector.push_back(inputVector.at(pTable[i]));
   }
-  return outputArray;
+  return outputVector;
 }
 
 
